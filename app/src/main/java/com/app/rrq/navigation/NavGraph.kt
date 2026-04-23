b2b3a9127224fb8e53d7d45174274035e4137812
Bachtiar <bahtiarvivo123@gmail.com>
2026-04-23 14:18:11 +0700

    bach

--- app/src/main/java/com/app/rrq/navigation/NavGraph.kt
+++ app/src/main/java/com/app/rrq/navigation/NavGraph.kt
@@ -1,2 +1,14 @@
 package com.app.rrq.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+@Composable
+fun AppNavHost(navController: NavHostController){
+
+}
