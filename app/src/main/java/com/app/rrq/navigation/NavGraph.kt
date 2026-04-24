cb45a0ffa43ff42ab70697ce1b6c1293def88ee1
Zahra-ayu <zahraayuazizah@gmail.com>
2026-04-24 10:51:38 +0700

    add detail laporan

--- app/src/main/java/com/app/rrq/navigation/NavGraph.kt
+++ app/src/main/java/com/app/rrq/navigation/NavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.app.rrq.DetailLaporan
 import com.app.rrq.HalamanAwal
 import com.app.rrq.SplashScreen
 
@@ -20,6 +21,9 @@ fun AppNavHost(navController: NavHostController){
         }
         composable("halamanAwal"){
             HalamanAwal(navController)
+        }
+        composable("detailLaporan"){
+            DetailLaporan(navController)
         }
     }
 }
